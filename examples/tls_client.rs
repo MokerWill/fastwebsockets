dcb87739f3dee98cdd64a94c3c8e2dbebd389cb6
moke <moke.nano.1314@gmail.com>
2025-02-10 19:30:56 +0800

    new banches; update dependencies

--- examples/tls_client.rs
+++ examples/tls_client.rs
@@ -5,6 +5,7 @@ use anyhow::Result;
 use bytes::Bytes;
 use fastwebsockets::FragmentCollector;
 use fastwebsockets::Frame;
+use fastwebsockets::handshake::client;
 use fastwebsockets::OpCode;
 use http_body_util::Empty;
 use hyper::header::CONNECTION;
@@ -12,79 +13,60 @@ use hyper::header::UPGRADE;
 use hyper::upgrade::Upgraded;
 use hyper::Request;
 use hyper_util::rt::TokioIo;
+use rustls_pki_types::ServerName;
 use tokio::net::TcpStream;
-use tokio_rustls::rustls::ClientConfig;
-use tokio_rustls::rustls::OwnedTrustAnchor;
+use tokio_rustls::rustls::{ClientConfig, RootCertStore};
 use tokio_rustls::TlsConnector;
 
 struct SpawnExecutor;
 
 impl<Fut> hyper::rt::Executor<Fut> for SpawnExecutor
 where
-  Fut: Future + Send + 'static,
-  Fut::Output: Send + 'static,
+    Fut: Future + Send + 'static,
+    Fut::Output: Send + 'static,
 {
   fn execute(&self, fut: Fut) {
     tokio::task::spawn(fut);
   }
 }
 
-fn tls_connector() -> Result<TlsConnector> {
-  let mut root_store = tokio_rustls::rustls::RootCertStore::empty();
+const UPSTREAM_DOMAIN: &str = "stream.binance.com";
 
-  root_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(
-    |ta| {
-      OwnedTrustAnchor::from_subject_spki_name_constraints(
-        ta.subject,
-        ta.spki,
-        ta.name_constraints,
-      )
-    },
-  ));
-
-  let config = ClientConfig::builder()
-    .with_safe_defaults()
-    .with_root_certificates(root_store)
-    .with_no_client_auth();
-
-  Ok(TlsConnector::from(Arc::new(config)))
-}
-
-async fn connect(domain: &str) -> Result<FragmentCollector<TokioIo<Upgraded>>> {
-  let mut addr = String::from(domain);
+async fn connect() -> Result<FragmentCollector<TokioIo<Upgraded>>> {
+  let mut addr = String::from(UPSTREAM_DOMAIN);
   addr.push_str(":9443"); // Port number for binance stream
 
-  let tcp_stream = TcpStream::connect(&addr).await?;
-  let tls_connector = tls_connector().unwrap();
-  let domain =
-    tokio_rustls::rustls::ServerName::try_from(domain).map_err(|_| {
-      std::io::Error::new(std::io::ErrorKind::InvalidInput, "invalid dnsname")
-    })?;
+  let new_domain = ServerName::try_from(UPSTREAM_DOMAIN)?;
+  let mut root_store = RootCertStore::empty();
+  root_store.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
 
-  let tls_stream = tls_connector.connect(domain, tcp_stream).await?;
+  let config = ClientConfig::builder()
+      .with_root_certificates(root_store)
+      .with_no_client_auth();
+  let tls_connector = TlsConnector::from(Arc::new(config));
+  let tcp_stream = TcpStream::connect(&addr).await?;
+  let tls_stream = tls_connector.connect(new_domain, tcp_stream).await?;
 
   let req = Request::builder()
-    .method("GET")
-    .uri(format!("wss://{}/ws/btcusdt@bookTicker", &addr)) //stream we want to subscribe to
-    .header("Host", &addr)
-    .header(UPGRADE, "websocket")
-    .header(CONNECTION, "upgrade")
-    .header(
-      "Sec-WebSocket-Key",
-      fastwebsockets::handshake::generate_key(),
-    )
-    .header("Sec-WebSocket-Version", "13")
-    .body(Empty::<Bytes>::new())?;
+      .method("GET")
+      .uri(format!("wss://{}/ws/btcusdt@bookTicker", &addr)) //stream we want to subscribe to
+      .header("Host", &addr)
+      .header(UPGRADE, "websocket")
+      .header(CONNECTION, "upgrade")
+      .header(
+        "Sec-WebSocket-Key",
+        fastwebsockets::handshake::generate_key(),
+      )
+      .header("Sec-WebSocket-Version", "13")
+      .body(Empty::<Bytes>::new())?;
 
-  let (ws, _) =
-    fastwebsockets::handshake::client(&SpawnExecutor, req, tls_stream).await?;
+  let (ws, _) = client(&SpawnExecutor, req, tls_stream).await?;
   Ok(FragmentCollector::new(ws))
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
-  let domain = "data-stream.binance.com";
-  let mut ws = connect(domain).await?;
+  let mut ws = connect().await?;
 
   loop {
     let msg = match ws.read_frame().await {
@@ -99,7 +81,7 @@ async fn main() -> Result<()> {
     match msg.opcode {
       OpCode::Text => {
         let payload =
-          String::from_utf8(msg.payload.to_vec()).expect("Invalid UTF-8 data");
+            String::from_utf8(msg.payload.to_vec()).expect("Invalid UTF-8 data");
         // Normally deserialise from json here, print just to show it works
         println!("{:?}", payload);
       }
